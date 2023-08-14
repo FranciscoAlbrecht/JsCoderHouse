089c40ea3a93a1f77e320d5e145287d978176edf
Francisco Albrecht <franciscoalbrecht.w@gmail.com>
2023-08-14 10:13:58 -0300

    push

--- js/main.js
+++ js/main.js
@@ -1,24 +1,3 @@
-// let opcion
-// do {
-//     let opcion = parseInt(prompt("Ingrese una opción:\n\n1. Ver productos\n2 Ver sucursales\n\n Para salir ingrese 0"));
-// switch (opcion) {
-//     case 0:
-//         alert("!Muchas por tu visita, vuelva pronto! ")
-//         break;
-//     case 1:
-//         alert("Estos son nuestros productos:")
-//         break;
-//     case 2:
-//         alert("Estas son nuestras sucursales:")
-//         break;
-
-//     default:
-//         alert("Opcion NO válida")
-// }
-
-// } while (opcion !== 0)
-
-
 let opcion = parseInt(prompt("Ingrese una opción:\n\n1. Ver productos\n2 Ver sucursales\n\n Para salir ingrese 0"));
 while (opcion !== 0) {
     if (opcion === 1) {
